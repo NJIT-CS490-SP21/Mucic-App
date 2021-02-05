1b06d8beb8bdfac2c7fb7d6dd6327aad49fe57aa
EC2 Default User <ec2-user@ip-172-31-53-6.ec2.internal>
2021-02-05 22:19:17 +0000

    added index.html and style.css also included flask functionality in app.py

--- app.py
+++ app.py
@@ -11,57 +11,67 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 from random import randint
+from flask import Flask, render_template
 
 ############################################# Authorization ####################################################
-load_dotenv(find_dotenv()) 
-AUTH_URL= "https://accounts.spotify.com/api/token" #url for authentication
+load_dotenv(find_dotenv())                                                                                     #
+AUTH_URL= "https://accounts.spotify.com/api/token" #url for authentication                                     #
+                                                                                                               #
+clientID = os.getenv("CLIENT_ID") #client id from the spotify api                                              #
+clientSecret = os.getenv("CLIENT_SECRET") #client secret from sptify api                                       #
+                                                                                                               #
+                                                                                                               #
+auth_response = requests.post(AUTH_URL, #authentication request                                                #
+{                                                                                                              #
+    'grant_type': 'client_credentials',                                                                        #
+    'client_id' :clientID,                                                                                     #
+    'client_secret' : clientSecret,                                                                            #
+}                                                                                                              #
+)                                                                                                              #
+auth_response_data = auth_response.json() #Respononse from the api                                             #
+auth_token = auth_response_data["access_token"] #authentication token                                          #
+header = { #authentication header for the get request                                                          #
+ 'Authorization': 'Bearer {token}'.format(token=auth_token)                                                    #
+ }                                                                                                             #
+################################################################################################################
 
-clientID = os.getenv("CLIENT_ID")
-clientSecret = os.getenv("CLIENT_SECRET")
 
-
-auth_response = requests.post(AUTH_URL,
-{
-    'grant_type': 'client_credentials',
-    'client_id' :clientID,
-    'client_secret' : clientSecret,
-}
-)
-auth_response_data = auth_response.json()
-auth_token = auth_response_data["access_token"]
-header = {
- 'Authorization': 'Bearer {token}'.format(token=auth_token)
- }
-##################################################################################################################
-
-###########################################     Song Info    #####################################################
-
-"""TRACK_URL = "https://api.spotify.com/v1/tracks/"
-song_id = "1ms5tNNdPouTtcD2oVF9Oz"
-params = {
-    "id" : song_id,
-    
-}
-response =  requests.get(TRACK_URL + song_id, headers = header)
-
-print(response.json()[])"""
-artists = ["137W8MRPWKqSmrBGDBFSop","3TVXtAsR1Inumwj472S9r4","45PG2L6Fh2XvYL4ONzpdoW"]
-selected_artist = artists[randint(0,3)]
-BASE_URL = "https://api.spotify.com/v1/artists/"
-params = { 
-            "market" : "US"
-            }
-FINAL_URL = BASE_URL
-response = requests.get(FINAL_URL+selected_artist+"/top-tracks", params = params,headers = header)
-
-artist_name = response.json()["tracks"][0]["artists"][0]['name']
-song_name = response.json()["tracks"][0]["name"]
-song_preview_url =response.json()["tracks"][0]["external_urls"]["spotify"]
-image_url =response.json()["tracks"][0]["album"]["images"][0]["url"]
-album_info = {
-    "song" : song_name,
-    "artist" : artist_name,
-    "song_url": song_preview_url,
-    "image_url" : image_url
-    }
-print(album_info)
+############################################          Creating the Flask App  ###############################################################################
+                                                                                                                                                            #
+app = Flask(__name__) # creating the app                                                                                                                    #
+@app.route("/") #route for the web app                                                                                                                      #
+def posting_song_info_on_the_webpage():                                                                                                                     #
+                                                                                                                                                            #
+#################################################             Song Info        ###############################################################              #
+    artists = ["137W8MRPWKqSmrBGDBFSop","3TVXtAsR1Inumwj472S9r4","45PG2L6Fh2XvYL4ONzpdoW"] #list of artist ids from spotify                  #              #
+                                                                                                                                             #              #
+    selected_artist = artists[randint(0,2)] #selecting random artist                                                                         #              #
+                                                                                                                                             #              #
+    BASE_URL = "https://api.spotify.com/v1/artists/" #first part of the url that will be used inextracting the data                          #              #
+    params = {                                                                                                                               #              #
+                "market" : "US" #parameter in the get request                                                                                #              #
+            }                                                                                                                                #              #
+                                                                                                                                             #              #
+    response = requests.get(BASE_URL+selected_artist+"/top-tracks", params = params,headers = header) #requesting data from the ai           #              #
+    song_index = randint(0,len(response.json()["tracks"])-1) #index of the song list to select random song                                   #              #
+    image_index = randint(0,len(response.json()["tracks"][song_index]["album"]["images"])-1) #index for selectinf random images from list    #              #                                      
+    artist_name =response.json()["tracks"][0]["artists"][0]['name'] #name of the artist                                                      #              #
+    song_name = response.json()["tracks"][song_index]["name"] #name of the song                                                              #              #
+    song_preview_url =response.json()["tracks"][song_index]["external_urls"]["spotify"] #link for the song                                   #              #
+    image_url =response.json()["tracks"][song_index]["album"]["images"][image_index]["url"] #link for the images                             #              #
+##############################################################################################################################################              #
+    return render_template("index.html", # sending data to index.html                                                                                       #
+    song_name = song_name,                                                                                                                                  #
+    artist_name = artist_name,                                                                                                                              #
+    song_preview_url = song_preview_url,                                                                                                                    #
+    image_url = image_url                                                                                                                                   #
+    )                                                                                                                                                       #
+                                                                                                                                                            #
+app.config['SEND_FILE_MAX_AGE_DEFAULT']=0 #code for updating style.css                                                                                      #
+app.run(        #running the application                                                                                                                    #
+    port = int(os.getenv("PORT",8080)),                                                                                                                     #
+    host = os.getenv("IP","0.0.0.0"),                                                                                                                       #
+    debug = True                                                                                                                                            #
+    )                                                                                                                                                       #
+                                                                                                                                                            #
+#############################################################################################################################################################
