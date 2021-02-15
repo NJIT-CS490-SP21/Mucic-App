727346d840ea76699900fef99ce7e53f4bf117e1
mindquaker <mindquaker20@gmail.com>
2021-02-15 05:44:18 +0000

    added code to get information in music.py and added different routes for each artist in app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from flask import Flask, render_template
 
 
 
+
 ############################################          Creating the Flask App  ###############################################################################
                                                                                                                                                             #
 app = Flask(__name__) # creating the app                                                                                                                    #
@@ -28,11 +29,94 @@ def posting_song_info_on_the_webpage():                                         
                                                                                                                                                             #
     )                                                                                                                                                       #
                                                                                                                                                             #
+                                                                                                                                                      #
+#############################################################################################################################################################
+
+                                                                                                                  #
+@app.route("/wiz-khalifa")
+def getting_wiz_info():
+    artist_info = music.get_artist_info("wiz khalifa")
+    return render_template("artist.html",
+    artist_name = artist_info["artist_name"],
+    total_followers = artist_info["total_followers"],
+    image_url = artist_info["image_url"],
+    song1_name = artist_info["song1"]["song_name"],
+    song1_preview_url = artist_info["song1"]["song_preview_url"],
+    song1_poster = artist_info["song1"]["song_poster"],
+    song1_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song2_name = artist_info["song2"]["song_name"],
+    song2_preview_url = artist_info["song2"]["song_preview_url"],
+    song2_poster = artist_info["song2"]["song_poster"],
+    song2_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song3_name = artist_info["song3"]["song_name"],
+    song3_preview_url = artist_info["song3"]["song_preview_url"],
+    song3_poster = artist_info["song3"]["song_poster"],
+    song3_lyric_link  = artist_info["song3"]["lyrics_url"]
+    )
+@app.route("/drake")
+def getting_drake_info():
+    artist_info = music.get_artist_info("drake")
+    return render_template("artist.html",
+    artist_name = artist_info["artist_name"],
+    total_followers = artist_info["total_followers"],
+    image_url = artist_info["image_url"],
+    song1_name = artist_info["song1"]["song_name"],
+    song1_preview_url = artist_info["song1"]["song_preview_url"],
+    song1_poster = artist_info["song1"]["song_poster"],
+    song1_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song2_name = artist_info["song2"]["song_name"],
+    song2_preview_url = artist_info["song2"]["song_preview_url"],
+    song2_poster = artist_info["song2"]["song_poster"],
+    song2_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song3_name = artist_info["song3"]["song_name"],
+    song3_preview_url = artist_info["song3"]["song_preview_url"],
+    song3_poster = artist_info["song3"]["song_poster"],
+    song3_lyric_link  = artist_info["song3"]["lyrics_url"]
+    )
+@app.route("/ed-sheeran")
+def getting_ed_info():
+    artist_info = music.get_artist_info("ed sheeran")
+    return render_template("artist.html",
+    artist_name = artist_info["artist_name"],
+    total_followers = artist_info["total_followers"],
+    image_url = artist_info["image_url"],
+    song1_name = artist_info["song1"]["song_name"],
+    song1_preview_url = artist_info["song1"]["song_preview_url"],
+    song1_poster = artist_info["song1"]["song_poster"],
+    song1_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song2_name = artist_info["song2"]["song_name"],
+    song2_preview_url = artist_info["song2"]["song_preview_url"],
+    song2_poster = artist_info["song2"]["song_poster"],
+    song2_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song3_name = artist_info["song3"]["song_name"],
+    song3_preview_url = artist_info["song3"]["song_preview_url"],
+    song3_poster = artist_info["song3"]["song_poster"],
+    song3_lyric_link  = artist_info["song3"]["lyrics_url"]
+    )
+@app.route("/maroon-5")
+def getting_maroon_info():
+    artist_info = music.get_artist_info("maroon 5")
+    return render_template("artist.html",
+    artist_name = artist_info["artist_name"],
+    total_followers = artist_info["total_followers"],
+    image_url = artist_info["image_url"],
+    song1_name = artist_info["song1"]["song_name"],
+    song1_preview_url = artist_info["song1"]["song_preview_url"],
+    song1_poster = artist_info["song1"]["song_poster"],
+    song1_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song2_name = artist_info["song2"]["song_name"],
+    song2_preview_url = artist_info["song2"]["song_preview_url"],
+    song2_poster = artist_info["song2"]["song_poster"],
+    song2_lyric_link  = artist_info["song2"]["lyrics_url"],
+    song3_name = artist_info["song3"]["song_name"],
+    song3_preview_url = artist_info["song3"]["song_preview_url"],
+    song3_poster = artist_info["song3"]["song_poster"],
+    song3_lyric_link  = artist_info["song3"]["lyrics_url"]
+    )
+    
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0 #code for updating style.css                                                                                      #
 app.run(        #running the application                                                                                                                    #
     port = int(os.getenv("PORT",8080)), # port number for the application                                                                                   #
     host = os.getenv("IP","0.0.0.0"),  #Ip address of the host                                                                                              #
     debug = True # to keep running the server                                                                                                               #
-    )                                                                                                                                                       #
-                                                                                                                                                            #
-#############################################################################################################################################################
+    ) 
--- music.py
+++ music.py
@@ -76,7 +76,7 @@ def get_lyrics(song_name,artist_name)  :                                        
 #################################################             Song Info        ###############################################################              
                                                                                                                                              #
 def extract_info():                                                                                                                          #
-    artists = ["137W8MRPWKqSmrBGDBFSop","3TVXtAsR1Inumwj472S9r4","6eUKZXaKkcviH0Ku9w2n3V","64KEffDW9EtZ1y2vBYgq8T"]                          #                          
+    artists = ["137W8MRPWKqSmrBGDBFSop","3TVXtAsR1Inumwj472S9r4","6eUKZXaKkcviH0Ku9w2n3V","04gDigrS5kc9YWfZHwBETP"]                          #                          
     #list of artist ids from spotify                                                                                                         #              
                                                                                                                                              #              
     selected_artist = artists[randint(0,3)] #selecting random artist                                                                         #              
@@ -97,7 +97,56 @@ def extract_info():                                                             
     info_dict ={"song_name": song_name,"artist_name": artist_name,"song_preview_url": song_preview_url, "image_url":image_url,               #
                 "lyrics_url":link_to_lyrics                                                                                                  #
     }                                                                                                                                        #
-    print(info_dict)                                                                                                                         #
+                                                                                                                             #
     return info_dict                                                                                                                         #
-##############################################################################################################################################   
-extract_info()
+############################################################################################################################################## 
+
+
+
+def get_artist_info(artist_name):
+    artists = ["137W8MRPWKqSmrBGDBFSop","3TVXtAsR1Inumwj472S9r4","6eUKZXaKkcviH0Ku9w2n3V","04gDigrS5kc9YWfZHwBETP"]                          #                          
+    if artist_name.lower() =="wiz khalifa":
+        artist_id = artists[0]
+    elif artist_name.lower() == "drake":
+        artist_id = artists[1]
+    elif artist_name.lower() == "ed sheeran":
+        artist_id = artists[2]
+    else:
+        artist_id = artists[3]
+    
+    BASE_URL="https://api.spotify.com/v1/artists"
+    params ={
+        "ids":artist_id,
+    }
+    
+    artist_response = requests.get(BASE_URL,params = params, headers = header)
+    artist_data =artist_response.json()
+    total_followers = artist_data["artists"][0]["followers"]["total"]
+    image_url = artist_data["artists"][0]["images"][0]["url"]
+    
+    song_url = "https://api.spotify.com/v1/artists/"+artist_id+"/top-tracks"
+    song_params= {
+        "market": "US"
+    }
+    song_response = requests.get(song_url, params = song_params,headers = header)
+    song_response_data = song_response.json()
+    
+    top_songs = {}
+    for i in range(3):
+        top_songs["song"+str(i+1)]={
+                "song_name" :song_response_data["tracks"][i]["name"],
+                "song_preview_url": song_response_data["tracks"][i]["preview_url"],
+                "song_poster":song_response_data["tracks"][i]["album"]["images"][0]["url"],
+                "lyrics_url" : get_lyrics(song_response_data["tracks"][i]["name"],artist_name)
+                
+            }
+    artist_info = {
+        "artist_name" :song_response_data["tracks"][0]["artists"][0]['name'],
+        "total_followers":total_followers,
+        "image_url" : image_url,
+        "song1" : top_songs["song1"],
+        "song2" : top_songs["song2"],
+        "song3" : top_songs["song3"]
+    } 
+    
+    return(artist_info)
